7e3b92a7d044699b554de65c51b1c122e9fd4049
eder24m3za <110887388+eder24m3za@users.noreply.github.com>
2022-12-08 14:51:17 -0600

    Update ada
    Ada

--- app/Http/Controllers/adafruitcontroller.php
+++ app/Http/Controllers/adafruitcontroller.php
@@ -6,5 +6,81 @@ use Illuminate\Http\Request;
 
 class adafruitcontroller extends Controller
 {
-    //
+    public function humedad (){
+        $response = Http::get("https://io.adafruit.com/api/v2/Abel0120/feeds/default-dot-humedad?X-AIO-KEY=aio_nFdz91zKSQCuOpVqj3rnGwU7XaS0",[]);
+
+        return response()->json([
+            "status"=>201,
+            "message"=>"Correcto",
+            "error"=>null,
+            "sensores"=>$response->object()->id,
+           "datas"=>$response->object()->last_value,
+            ],201);
+        }  
+        
+        public function luz (){
+            $response = Http::get("https://io.adafruit.com/api/v2/Abel0120/feeds/default-dot-luz?X-AIO-KEY=aio_nFdz91zKSQCuOpVqj3rnGwU7XaS0",[]);
+    
+            return response()->json([
+                "status"=>201,
+                "message"=>"Correcto",
+                "error"=>null,
+                "sensores"=>$response->object()->id,
+               "datas"=>$response->object()->last_value,
+                ],201);
+            } 
+
+            public function corrientemax (){
+                $response = Http::get("https://io.adafruit.com/api/v2/Abel0120/feeds/default-dot-imax?X-AIO-KEY=aio_nFdz91zKSQCuOpVqj3rnGwU7XaS0",[]);
+        
+                return response()->json([
+                    "status"=>201,
+                    "message"=>"Correcto",
+                    "error"=>null,
+                    "sensores"=>$response->object()->id,
+                   "datas"=>$response->object()->last_value,
+                    ],201);
+                } 
+
+                public function corrientemin (){
+                    $response = Http::get("https://io.adafruit.com/api/v2/Abel0120/feeds/default-dot-imin?X-AIO-KEY=aio_nFdz91zKSQCuOpVqj3rnGwU7XaS0",[]);
+            
+                    return response()->json([
+                        "status"=>201,
+                        "message"=>"Correcto",
+                        "error"=>null,
+                        "sensores"=>$response->object()->id,
+                       "datas"=>$response->object()->last_value,
+                        ],201);
+                    } 
+
+                    public function corrientewatts (){
+                        $response = Http::get("https://io.adafruit.com/api/v2/Abel0120/feeds/default-dot-watts?X-AIO-KEY=aio_nFdz91zKSQCuOpVqj3rnGwU7XaS0",[]);
+                
+                        return response()->json([
+                            "status"=>201,
+                            "message"=>"Correcto",
+                            "error"=>null,
+                            "sensores"=>$response->object()->id,
+                           "datas"=>$response->object()->last_value,
+                            ],201);
+                        } 
+
+                        public function temperatura (){
+                            $response = Http::get("https://io.adafruit.com/api/v2/Abel0120/feeds/default-dot-temperatura?X-AIO-KEY=aio_nFdz91zKSQCuOpVqj3rnGwU7XaS0",[]);
+                    
+                            return response()->json([
+                                "status"=>201,
+                                "message"=>"Correcto",
+                                "error"=>null,
+                                "sensores"=>$response->object()->id,
+                               "datas"=>$response->object()->last_value,
+                                ],201);
+                            } 
+
+
+
+
+
+
 }
